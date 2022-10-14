4b89ed956f5569088de1bd177042f08e5d9139fa
jaikesJT <jakob.tossebro@gmail.com>
2022-10-14 14:36:23 +0200

    renamed to BoxScript

--- Assets/Scripts/BoxScript.cs
+++ Assets/Scripts/BoxScript.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StructureScript : MonoBehaviour
+public class BoxScript : MonoBehaviour
 {   
 
     void Awake()
     {   
         gameObject.AddComponent<BoxCollider2D>();
+        
     }
 
 }
